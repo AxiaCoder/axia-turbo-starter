e7268dcd4df726f72d84edac2af0a94bcb71a061
Benoît G <uccellounet@hotmail.fr>
2023-02-17 15:40:59 +0100

    fix(core/i18n): simplify i18n library and add sample directory

--- packages/i18n/en/index.ts
+++ packages/i18n/en/index.ts
@@ -1,7 +1,5 @@
-import test from "./test.json";
 import front from "./front.json";
 
 export const en = {
-  test,
   front,
 };
--- packages/i18n/fr/index.ts
+++ packages/i18n/fr/index.ts
@@ -1,7 +1,5 @@
-import test from "./test.json";
 import front from "./front.json";
 
 export const fr = {
-  test,
   front,
 };
